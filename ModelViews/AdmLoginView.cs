2c3b6e1b15051094d699668549c9e08316366157
João Paulo Ghissardi da Silva <jp17paulo@terra.com.br>
2022-04-29 12:09:18 -0300

    Versionando o projeto

---
+++ ModelViews/AdmLoginView.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace api_desafio21dias.ModelViews
+{
+    public record AdmLoginView
+    {
+        public string Email {get; set;}
+        
+        public string Senha {get; set;}
+    }
+}
